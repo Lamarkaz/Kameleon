235584cceafedf65983a0876eaa2cc87d3e59dcd
nourharidy <nourharidy@yahoo.com>
2018-12-21 14:57:18 +0200

    removed console.logs

--- src/abci.js
+++ src/abci.js
@@ -50,7 +50,6 @@ module.exports = function (app) {
       app.stateMachine.transition({ type: 'block', data: {} })
       let { validators } = app.stateMachine.context()
       let validatorUpdates = []
-      console.log(validators)
       for (let pubKey in validators) {
         validatorUpdates.push({
           pubKey: { type: validators[pubKey].type, data: Buffer.from(pubKey, 'base64') },
@@ -69,7 +68,6 @@ module.exports = function (app) {
       return { data: Buffer.from(data, 'hex') }
     },
     initChain(request) {
-      console.log(request.validators)
       /**
        * in next abci version, we'll get a timestamp here.
        * height is no longer tracked on info (we want to encourage isomorphic chain/channel code)
